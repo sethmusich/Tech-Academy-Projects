5126e9d396a8f196505111da14d30a0d9c25da40
sethmusich <sethmusich@gmail.com>
2017-07-31 13:48:15 -0400

    Simple program that calculates shipping costs

---
+++ PostalCalcChallenge/PostalCalcChallenge/default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="PostalCalcChallenge._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator<br />
+            <br />
+            Width:<asp:TextBox ID="widthBox" runat="server" AutoPostBack="true" OnTextChanged="handleChange"></asp:TextBox>
+            <br />
+            Height:<asp:TextBox ID="heightBox" runat="server" AutoPostBack="true" OnTextChanged="handleChange"></asp:TextBox>
+            <br />
+            Length:<asp:TextBox ID="lengthBox" runat="server" AutoPostBack="true" OnTextChanged="handleChange"></asp:TextBox>
+            <br />
+        </div>
+        <asp:RadioButton ID="groundButton" runat="server" AutoPostBack="true" OnCheckedChange="handleChange" Text="Ground" GroupName="buttons" />
+        <br />
+        <asp:RadioButton ID="airButton" runat="server" AutoPostBack="true" OnCheckedChanged="handleChange" Text="Air" GroupName="buttons" />
+        <br />
+        <asp:RadioButton ID="nextDayButton" runat="server" AutoPostBack="true" OnCheckedChanged="handleChange" Text="Next Day" GroupName="buttons" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ PostalCalcChallenge/PostalCalcChallenge/default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PostalCalcChallenge
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void handleChange(object sender, EventArgs e)
+        {
+            // do the values in the textboxes exist
+            if (!valuesExist()) return;
+
+            // what is the volume?
+            int volume = 0;
+            if (!tryGetVolume(out volume)) return;
+
+            // what is the mult
+            double postageMultiplier = getPostageMultiplier();
+
+            // what is the cost (vol x mult.)
+            double cost = postageMultiplier * volume;
+
+            //show the user 
+            resultLabel.Text = String.Format("Your total cost is {0:C} to ship", cost);
+
+        }
+
+        private bool valuesExist()
+        {
+            if (!airButton.Checked
+                && !groundButton.Checked
+                && !nextDayButton.Checked)
+                return false;
+
+            if (widthBox.Text.Trim().Length == 0
+                || heightBox.Text.Trim().Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private bool tryGetVolume(out int volume)
+        {
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            if (!int.TryParse(widthBox.Text.Trim(), out width))
+                return false;
+            if (!int.TryParse(heightBox.Text.Trim(), out height))
+                return false;
+            if (!int.TryParse(lengthBox.Text.Trim(), out length))
+                length = 1;
+
+            volume = length * width * height;
+
+            return true;    
+        }
+
+        private double getPostageMultiplier()
+        {
+            if (groundButton.Checked) return .15;
+            else if (airButton.Checked) return .25;
+            else if (nextDayButton.Checked) return .45;
+            else return 0;
+              
+        }
+
+
+    }
+}
