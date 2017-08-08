7dbc0c5aae971433e9aec6731ac89bb7a5be9d4c
sethmusich <sethmusich@gmail.com>
2017-08-08 16:55:27 -0400

    Added simple darts project

---
+++ SimpleDarts/SimpleDarts/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleDarts.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ SimpleDarts/SimpleDarts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace SimpleDarts
+{
+    public class Score
+    {
+        public static void ScoreDart(Player player, Dart dart)
+        {
+            int score = 0;
+
+            if (dart.IsTriple) score = dart.Score * 3;
+            else if (dart.IsTriple) score = dart.Score * 3;
+            else score = dart.Score;
+
+            if (dart.IsTriple && dart.Score == 0) score = 50;
+            else if (dart.Score == 0) score = 25;
+
+            player.Score += score;
+        }
+    }
+}
