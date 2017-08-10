6d238c7bc7c03f72badfe910e5c5716556f84f9f
sethmusich <sethmusich@gmail.com>
2017-08-10 14:16:27 -0400

    initial commit

---
+++ MegaChallengeWar/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaChallengeWar.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            Play War!<br />
+        </div>
+        <asp:Button ID="playButton" runat="server" OnClick="playButton_Click" Text="Play!" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ MegaChallengeWar/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeWar
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void playButton_Click(object sender, EventArgs e)
+        {
+            
+            Game game = new Game("Player 1", "Player 2");
+            resultLabel.Text = game.Play();
+          
+        }
+    }
+}
