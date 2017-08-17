05bdba145c51b113f3b019290fac057723326c44
sethmusich <sethmusich@gmail.com>
2017-08-17 14:42:35 -0400

    no message

---
+++ rateCalculator/rateCalculator/default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="rateCalculator._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        #form1 {
+            text-align: center;
+        }
+        .auto-style2 {
+            font-size: large;
+        }
+        .auto-style3 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style3">
+            <span class="auto-style1">Welcome to the Common Census Rate Calculation Tool<br />
+            <br />
+            </span>
+            <span class="auto-style2">Select Your Options Below to Calculate the Cost of your Plan<br />
+            </span><br />
+            <br />
+            <strong>Choose your Plan Type:</strong> <asp:RadioButton ID="bronzeButton" runat="server" GroupName="Plan" Text="Bronze" />
+            &nbsp;
+            <asp:RadioButton ID="silverButton" runat="server" GroupName="Plan" Text="Silver" />
+            &nbsp;
+            <asp:RadioButton ID="goldButton" runat="server" GroupName="Plan" Text="Gold" />
+        </div>
+        <p class="auto-style3">
+            <strong>Choose your Coverage Level:</strong>
+            <asp:RadioButton ID="EE" runat="server" GroupName="Coverage" Text="EE (Single)" />
+            &nbsp;
+            <asp:RadioButton ID="EESpouse" runat="server" GroupName="Coverage" Text="EE + Spouse" />
+            &nbsp;
+            <asp:RadioButton ID="EEChildren" runat="server" GroupName="Coverage" Text="EE + Children" />
+            &nbsp;
+            <asp:RadioButton ID="EEFamily" runat="server" GroupName="Coverage" Text="EE + Family" />
+        </p>
+        <p class="auto-style3">
+            <strong>Frequency (in Months):</strong>
+            <asp:TextBox ID="freqBox" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style3">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calculate" />
+        </p>
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ rateCalculator/rateCalculator/default.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace rateCalculator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+            double rate;
+
+            //if statement to calculate through every option
+            //typically, I would would iterate through options either with a seperate classes for each 
+            //or a for loop but this is a small app with 12, non-expandable options. 
+
+            if (goldButton.Checked && EE.Checked)
+                rate = 14.02;
+
+            else if (goldButton.Checked && EESpouse.Checked)
+                rate = 24.12;
+
+            else if (goldButton.Checked && EEChildren.Checked)
+                rate = 29.17;
+            else if (goldButton.Checked && EEFamily.Checked)
+                rate = 30.56;
+    
+            else if (silverButton.Checked && EE.Checked)
+                rate = 12.26;
+
+            else if (silverButton.Checked && EESpouse.Checked)
+                rate = 23.43;
+
+            else if (silverButton.Checked && EEChildren.Checked)
+                rate = 24.92;
+            else if (silverButton.Checked && EEFamily.Checked)
+                rate = 29.02;
+
+            else if (bronzeButton.Checked && EE.Checked)
+                rate = 9.6;
+
+            else if (bronzeButton.Checked && EESpouse.Checked)
+                rate = 19.32;
+
+            else if (bronzeButton.Checked && EEChildren.Checked)
+                rate = 20.12;
+            else rate = 25.16;
+
+
+
+            //exception handler to catch non-numeric entries
+
+            int frequency = 0;
+            bool isNumber = false;
+
+            isNumber = int.TryParse(freqBox.Text, out frequency);
+
+            if (!isNumber)
+            {
+                resultLabel.Text += "Please use a numeric value.";
+            }
+            else
+            {
+                frequency = int.Parse(freqBox.Text);
+            }
+
+            //calculating yearly and monthly cost, and converting montly rate from double to int
+
+      
+            double yearRate = rate * 12;
+            double cost = frequency * rate;
+
+           
+
+            //print result
+
+            resultLabel.Text = String.Format("Your cost is: <br> {0:C2} per month <br>{1:C2} per year <br>{2:C2} for the selected amount of months", rate, yearRate, cost);
+
+
+        }
+    }
+}
